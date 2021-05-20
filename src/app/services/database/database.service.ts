0f6df5742e7dec8de6ccf57e95e08720d81b70f5
julia <s1710456010@students.fh-hagenberg.at>
2021-05-20 19:17:52 +0200

    save user

--- src/app/services/database/database.service.ts
+++ src/app/services/database/database.service.ts
@@ -1,9 +1,29 @@
+import { AngularFireDatabase } from '@angular/fire/database';
+
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/shared/types/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
 
-  constructor() { }
+  public db;
+
+  constructor(db: AngularFireDatabase) {
+    this.db = db;
+  }
+
+
+  public async saveUser(user : User): Promise<void> {
+    console.log(user.id);
+    const itemRef = this.db.object('/users/' + user.id);
+    return itemRef.set(user)
+        .then(()=>{
+            console.log("created new User");
+        })
+        .catch(error =>{
+            console.error(error + "no user created")
+        })
+  }
 }
